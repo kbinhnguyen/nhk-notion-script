4f3c7c610c6c7eafc46b4de85fc5bdd4be6bc093
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-09-25 22:52:57 -0400

    Retrieved page content from Notion

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
+
+// get page info
+// (async () => {
+//   const pageId = '3a7aae14-f423-4a78-b1c7-8ee1fe584fd7';
+//   const response = await notion.pages.retrieve({ page_id: pageId });
+//   console.log(response);
+// })();
+
+
+// get page content
 (async () => {
-  const pageId = '3a7aae14-f423-4a78-b1c7-8ee1fe584fd7';
-  const response = await notion.pages.retrieve({ page_id: pageId });
-  console.log(response);
+  const blockId = '3a7aae14-f423-4a78-b1c7-8ee1fe584fd7';
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  console.log(response.results[1].paragraph.rich_text[0].text);
 })();
 
