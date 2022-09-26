d4f5eac5d2e19f20861afeaf8f548b848af5a8bc
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-09-26 03:49:08 -0400

    Page creation works well with nice formatting for multiple body sections as well as no first subheader

--- index.js
+++ index.js
@@ -16,15 +16,132 @@ async function getHtml(url) {
     responseType: 'document',
   });
 
+
   const $ = cheerio.load(data);
-  const time = $('time').text();
-  const coverImg = $('img', '.content--thumb').attr('data-src');
-  const summary = $('.content--summary').html().split('<br>').join('\n\n');
-  console.log(summary);
+  // console.log($('iframe'));
+
+   let notionPageElements = [{
+    object: 'block',
+    paragraph: {
+      rich_text: [
+        {
+          text: {
+            content: $('time').text(),
+          },
+        }
+      ],
+      color: 'gray',
+    },
+  },
+  {
+    object: 'block',
+    callout: {
+      rich_text: [
+        {
+          text: {
+            content: $('.content--summary').html().split('<br>').join('\n'),
+          },
+        },
+      ],
+      icon: {
+      emoji: '⭐'
+      },
+      color: 'gray_background',
+    },
+  }];
+
+  const expandedSummary = $('.content--summary-more');
+  if (expandedSummary.length === 1) {
+    notionPageElements.push({
+      object: 'block',
+      paragraph: {
+        rich_text: [
+          {
+            text: {
+              content: ' ',
+            },
+          },
+        ],
+      },
+    },
+    {
+      object: 'block',
+      paragraph: {
+        rich_text: [
+          {
+            text: {
+              content: expandedSummary.html().split('<br><br>').join('\n\n'),
+            },
+          },
+        ],
+      },
+    });
+  }
+
+  const bodySections = $('.content--body');
+
+  if (bodySections.length > 0) {
+    bodySections.each((index, element) => {
+      notionPageElements.push({
+        object: 'block',
+        heading_1: {
+          rich_text: [
+            {
+              text: {
+                content: $('.body-title', element).text(),
+              }
+            }
+          ]
+        },
+      },
+      {
+        object: 'block',
+        paragraph: {
+          rich_text: [
+            {
+              text: {
+                content: $('.body-text', element).html().split('<br><br>').join('\n\n'),
+              },
+            },
+          ],
+        },
+      });
+    });
+  }
+
+
+  const response = await notion.pages.create({
+    cover: {
+      type: 'external',
+      external: {
+        url: `https://www3.nhk.or.jp${$('img', '.content--thumb').attr('data-src')}`,
+      }
+    },
+    parent: {
+      type: 'database_id',
+      database_id: '145670bfa33e424c98aad0f7045ddcc9',
+    },
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: $('span', '.content--title').text(),
+              link: { url },
+            }
+          }
+        ]
+      },
+    },
+    children: notionPageElements,
+  });
+  console.log(response);
+
 };
 
 
-getHtml('https://www3.nhk.or.jp/news/html/20220926/k10013837341000.html');
+// getHtml('https://www3.nhk.or.jp/news/html/20220926/k10013837341000.html');
+getHtml('https://www3.nhk.or.jp/news/html/20220926/k10013837351000.html');
 
 
 //get page info
@@ -56,131 +173,3 @@ getHtml('https://www3.nhk.or.jp/news/html/20220926/k10013837341000.html');
 //   console.log(response);
 // })();
 
-
-// create a new page in a database
-/*
-(async () => {
-  const response = await notion.pages.create({
-    "cover": {
-        "type": "external",
-        "external": {
-            "url": "https://www3.nhk.or.jp/news/html/20220926/K10013837341_2209261256_0926125943_01_02.jpg"
-        }
-    },
-    "parent": {
-        "type": "database_id",
-        "database_id": "145670bfa33e424c98aad0f7045ddcc9"
-    },
-    "properties": {
-        "Name": {
-            "title": [
-                {
-                    "text": {
-                        "content": "【随時更新】安倍元首相 国葬 まもなく弔問外交始まる",
-                        "link": {
-                          "url": 'https://www3.nhk.or.jp/news/html/20220926/k10013837341000.html',
-                        }
-                    }
-                }
-            ]
-        },
-    },
-    "children": [
-        {
-          "object": "block",
-          "paragraph": {
-              "rich_text": [
-                  {
-                      "text": {
-                          "content": "2022年9月26日 12時20分",
-                      },
-
-                  }
-              ],
-              "color": "gray"
-            }
-        },
-        {
-            "object": "block",
-            "callout": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": "安倍元総理大臣の「国葬」に伴う「弔問外交」がまもなく始まります。\n\n国葬の最新ニュースをこの記事で随時お届けします。",
-                        },
-                    },
-                ],
-                "icon": {
-                  "emoji": "⭐"
-                },
-                // "children": [
-                  // {
-                  //   "object": "block",
-                  //   "paragraph": {
-                  //       "rich_text": [
-                  //           {
-                  //               "text": {
-                  //                   "content": "安倍元総理大臣の「国葬」に伴う「弔問外交」がまもなく始まります。",
-                  //               },
-
-                  //           }
-                  //       ],
-                  //     }
-                  //   },
-                //     {
-                //       "object": "block",
-                //       "paragraph": {
-                //           "rich_text": [
-                //               {
-                //                   "text": {
-                //                       "content": "国葬の最新ニュースをこの記事で随時お届けします。",
-                //                   },
-
-                //               }
-                //           ],
-                //         }
-                //     },
-                // ],
-                "color": "gray_background",
-            }
-        },
-        {
-          "object": "block",
-          "heading_1": {
-              "rich_text": [
-                  {
-                      "text": {
-                          "content": "安倍元総理大臣の「国葬」に伴う「弔問外交」がまもなく始まります。"
-                      }
-                  }
-              ]
-          }
-        },
-        {
-          "object": "block",
-          "paragraph": {
-              "rich_text": [
-                  {
-                      "text": {
-                          "content": "安倍元総理大臣の「国葬」に伴う「弔問外交」がまもなく始まります。\n\n初日の26日、岸田総理大臣は、アメリカのハリス副大統領らと会談することにしています。\n\n安倍元総理大臣の「国葬」に参列するため、日本を訪れた海外の要人との「弔問外交」は、26日から28日まで東京 港区の迎賓館で行われます。",
-                      },
-                  },
-                  // {
-                  //   "text": {
-                  //       "content": "初日の26日、岸田総理大臣は、アメリカのハリス副大統領らと会談することにしています。",
-                  //   },
-                  // },
-                  // {
-                  //   "text": {
-                  //       "content": "安倍元総理大臣の「国葬」に参列するため、日本を訪れた海外の要人との「弔問外交」は、26日から28日まで東京 港区の迎賓館で行われます。",
-                  //   },
-                  // }
-              ],
-
-          }
-      },
-    ]
-});
-  console.log(response);
-})();
-*/
